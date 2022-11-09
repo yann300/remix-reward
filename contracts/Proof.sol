97939f5c971337b9558f43a5cb2f97b5260790ad
yann300 <yann.levreau@gmail.com>
2022-11-09 12:27:46 +0100

    use OZ 4.7.3 version & add publishChallenge function

---
+++ contracts/Proof.sol
@@ -0,0 +1,16 @@
+library ZKVerifier {
+    struct G1Point {
+        uint X;
+        uint Y;
+    }
+    // Encoding of field elements is: X[0] * z + X[1]
+    struct G2Point {
+        uint[2] X;
+        uint[2] Y;
+    }
+    struct Proof {
+        G1Point a;
+        G2Point b;
+        G1Point c;
+    }
+}
