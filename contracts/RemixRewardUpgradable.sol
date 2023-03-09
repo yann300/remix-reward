c5f84aa1a7511896afc850afe25bf92166157fd6
yann300 <yann.levreau@gmail.com>
2023-03-09 16:09:30 +0100

    fix ref

--- contracts/RemixRewardUpgradable.sol
+++ contracts/RemixRewardUpgradable.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable@4.7.3/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.7.3/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable@4.7.3/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.7.3/proxy/utils/UUPSUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.7.3/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.7.3/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.7.3/utils/CountersUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable@4.7.3/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 import "./Proof.sol";
 
